00818d49d66a3fed36efd56ee26437b5553c1a88
Haron1988 <haron0505@gmail.com>
2022-02-05 18:06:02 +0200

    Haron1988
    basic structure

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="wrapper">
+        <header class="header">
+
+        </header>
+
+        <div class="section">
+            <nav class="nav">
+                <ul class="navigation__list">
+                    <li class="navigation__item"><a class="navigation__link" href="index.php" >Главная</a></li>
+                    <li class="navigation__item"><a class="navigation__link" href="lesson1.php" >1 урок. Таблица умножения</a></li>
+                    <li class="navigation__item"><a class="navigation__link" href="lesson2.php" >2 урок. Таблица умножения. Разукрашенная</a></li>
+                    <li class="navigation__item"><a class="navigation__link" href="lesson3.php" >3 урок. Форма + реверс</a></li>
+                </ul>
+            </nav>
+            <main class="main__content">
+
+            </main>
+        </div>
+        <footer class="footer">
+
+        </footer>
+    </div>
+    
+</body>
+</html>
